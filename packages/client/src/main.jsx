112e02fbe195c2a7c80bfafff37ca5ff19e8a119
Youssef548 <youssefahmedpvp@gmail.com>
2026-03-27 15:49:51 +0200

    feat: wrap app with ThemeProvider

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
-import ErrorBoundary from './components/ErrorBoundary';
 import './main.css';
 
 const queryClient = new QueryClient({
@@ -17,11 +17,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <ErrorBoundary>
+        <ThemeProvider>
+          <AuthProvider>
             <App />
-          </ErrorBoundary>
-        </AuthProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
